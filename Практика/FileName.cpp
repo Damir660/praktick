7c520c4dc2b463cbaab5f7a8c5a815ecc9102c94
Damir <damir_tinchurin@mail.ru>
2025-07-08 00:33:18 +0300

    Добавлено меню выбора режима

--- Практика/FileName.cpp
+++ Практика/FileName.cpp
@@ -1,9 +1,47 @@
+#include <conio.h>
 #include <iostream>
 #include <locale.h>
 #include <windows.h>
+#include <cstring>
+#include <fstream>
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    printf("Программа для алгоритма Прима\n");
-    return 0;
-}
+using namespace std;
+
+#define INF 9999999
+
+
+FILE* file = fopen("text.txt", "a");
+
+
+int main()
+{
+
+	srand(time(0));
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int** matr; //указатель для массива указателей 
+	int* selected; // Массив для отслеживания вершины
+	int* stp;
+	int n, d;
+
+
+	printf(" Добро пожаловать! Выберите один из пунктов меню, что начать: \n\n");
+	printf(" 1. Автоматическая генерация матрицы. (нажмите 1) \n");
+	printf(" 2. Ручной ввод (нажмите 2) \n");
+	printf(" 3. Завершить сеанс (нажмите 3)\n");
+	printf("=========================================================================");
+
+	d = _getch();
+
+	if (d == '1')
+	{
+		printf("\n");
+		printf("Введите размер матрицы: ");
+
+		scanf("%d", &n);
+
+		printf("=========================================================================");
+		return 0;
+	}
